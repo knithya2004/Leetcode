2bc48b28c2c97a0657386819d944e6d3e59b9b5a
knithya2004 <132334432+knithya2004@users.noreply.github.com>
2026-09-25 08:31:00 +0530

    Time: 10 ms (36.40%) | Memory: 63.3 MB (6.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,29 +1,26 @@
-import java.util.Arrays;
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int count=1;
+        int max=1;
+        int val=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                count=1;
+            }
+            if(nums[i]==nums[i-1])
+            {
+                count++;
+            }
+            if(count>max)
+            {
+                val=nums[i];
+                max=count;
+            }
 
-class Solution { 
-    public int majorityElement(int[] nums) { 
-        Arrays.sort(nums); 
-        int count=1; 
-        int max=1; 
-        int val=nums[0]; 
-        
-        for(int i=1;i<nums.length;i++) 
-        { 
-            if(nums[i]!=nums[i-1]) 
-            { 
-                count=1; 
-            } 
-            if(nums[i]==nums[i-1]) 
-            { 
-                count++; 
-            } 
-            if(count>max) 
-            { 
-                val=nums[i]; 
-                max=count; 
-            } 
-        } 
-        
-        return val; 
-    } 
+        }
+        return val;
+    }
 }
