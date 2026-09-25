e6f8c8832237c3319abd38082617bdb623b17863
knithya2004 <132334432+knithya2004@users.noreply.github.com>
2026-09-25 08:28:48 +0530

    Time: 11 ms (35.85%) | Memory: 63.6 MB (6.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,29 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-    }
+import java.util.Arrays;
+
+class Solution { 
+    public int majorityElement(int[] nums) { 
+        Arrays.sort(nums); 
+        int count=1; 
+        int max=1; 
+        int val=nums[0]; 
+        
+        for(int i=1;i<nums.length;i++) 
+        { 
+            if(nums[i]!=nums[i-1]) 
+            { 
+                count=1; 
+            } 
+            if(nums[i]==nums[i-1]) 
+            { 
+                count++; 
+            } 
+            if(count>max) 
+            { 
+                val=nums[i]; 
+                max=count; 
+            } 
+        } 
+        
+        return val; 
+    } 
 }
